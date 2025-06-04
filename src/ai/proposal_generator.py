865357134bff3d6ca8e0e52d5a2a384e7d7f7ce3
Prachi Pandey <131760466+prachi-pandey-github@users.noreply.github.com>
2025-06-05 04:18:04 +0530

    Update proposal_generator.py

--- src/ai/proposal_generator.py
+++ src/ai/proposal_generator.py
@@ -4,11 +4,11 @@ class ProposalGenerator:
         self.gemini_api = gemini_api
 
     def generate_proposal(self, client_requirements):
-        processed_requirements = self.langchain_utils.process_requirements(client_requirements)
-        proposal = self.gemini_api.send_request(processed_requirements)
+        processed_requirements = self.langchain_utils.process_client_requirements(client_requirements)
+        proposal = self.gemini_api.generate_proposal(processed_requirements)
         return proposal
 
     def generate_pitch_deck(self, client_requirements):
-        processed_requirements = self.langchain_utils.process_requirements(client_requirements)
-        pitch_deck = self.gemini_api.send_request(processed_requirements, pitch_deck=True)
+        processed_requirements = self.langchain_utils.process_client_requirements(client_requirements)
+        pitch_deck = self.gemini_api.generate_pitch_deck(processed_requirements)
         return pitch_deck
